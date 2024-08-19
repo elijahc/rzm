ee8af67aec1dffa51f7439edcd6c95cb95e5ec8e
Elijah Christensen <ejd.christensen@gmail.com>
2024-08-18 18:40:28 -0600

    Quartz sync: Aug 18, 2024, 6:40 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "rhizome 🪴",
+    pageTitle: "rhizome",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
